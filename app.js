514015696cd25d6b4ab9e0a139a5c0dacee19cf3
gabaal <geraldbaalham@live.co.uk>
2024-01-16 11:25:52 +0000

    Workshop finished

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log("Working");
+const blogPost = {
+  title: "My first blog post",
+  author: {
+    name: "Bob",
+    age: 30,
+    favouriteColour: "red",
+  },
+  tags: ["coding", "javascript", "objects", "arrays"],
+  content: "This is my blog post about learning objects and arrays.",
+};
+console.log(blogPost);
+
+console.log(blogPost.author.name); // Output: Bob
+
+console.log(blogPost.tags[0]); // Output: coding
+
+console.log(blogPost.tags.length); // Output: 4
+console.log(
+  `There are ${blogPost.tags.length} tags for post "${blogPost.title}":`
+); // Output: There are 4 tags for post "My first blog post":
+for (let i = 0; i < blogPost.tags.length; i++) {
+  console.log(blogPost.tags[i]); // Output: coding, javascript, objects, arrays
+}
+
+console.log(`Tags for post "${blogPost.title}":`); // Output: Tags for post "My first blog post":
+for (let tag of blogPost.tags) {
+  console.log(tag); // Output: coding, javascript, objects, arrays
+}
